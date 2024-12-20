2eee926744ef405a45be8e2bdf5d28278ca0590d
Charlotte Brown <charlotte.brown@marks-and-spencer.com>
2024-12-20 16:05:45 +0000

    Implement diagonals and columns

--- connect4.py
+++ connect4.py
@@ -1,3 +1,5 @@
+from typing import Tuple
+
 def init_board(rows: int, cols: int) -> list:
     board = []
     for _ in range(rows):
@@ -8,16 +10,92 @@ def init_board(rows: int, cols: int) -> list:
     return board
 
 
-def place_piece(player: str, row: int, col: int, board: list):
+def place_piece(player: str, row: int, col: int, board: list) -> list:
     if row >= len(board):
-        return
+        return None
     if col >= len(board[row]):
-        return
+        return None
     if board[row][col] != '.':
-        return
+        return None
     board[row][col] = player
     return board
 
+
+def check_sequence(seq: list) -> str:
+    result = "".join(seq).find("xxxx")
+    if result == -1:
+        o_result = "".join(seq).find("oooo")
+        if o_result != -1:
+            return 'o'
+    else:
+        return 'x'
+
+
+def get_col_diag_indexes(row: list, row_index: int, grid_height: int, connect_length: int) -> Tuple[list, list]:
+    row_str = "".join(row)
+    first_x = row_str.find("x")
+    first_o = row_str.find("o")
+    print("row_id: {}, first_o: {}".format(row_index, first_o))
+    row_indices_x = []
+    row_indices_o = []
+    diag_indices_x = []
+    diag_indices_o = []
+    if first_x != -1:
+        stop = connect_length + row_index
+        if stop <= grid_height:
+            for i in range(connect_length):
+                row_indices_x.append((i + row_index, first_x))
+
+                col = first_x + i
+                if col < len(row):
+                    diag_indices_x.append((i + row_index, col))
+    if first_o != -1:
+        stop = connect_length + row_index
+        if stop <= grid_height:
+            for i in range(connect_length):
+                row_indices_o.append((i + row_index, first_o))
+                col = first_o + i
+                if col < len(row):
+                    diag_indices_o.append((i + row_index, col))
+    return row_indices_x, row_indices_o, diag_indices_x, diag_indices_o
+
+
+def check_col(board: list, row_index: int, connect_length: int) -> str:
+    x_indices, o_indices, x_diag, o_diag = get_col_diag_indexes(board[row_index], row_index, len(board), connect_length)
+    print(x_diag)
+    word = []
+    diag_word = []
+    if len(x_indices) >= connect_length:
+        for coord in x_indices:
+            word.append(board[coord[0]][coord[1]])
+    if len(o_indices) >= connect_length:
+        for coord in o_indices:
+            word.append(board[coord[0]][coord[1]])
+    if len(x_diag) >= connect_length:
+        for coord in x_diag:
+            diag_word.append(board[coord[0]][coord[1]])
+    if len(o_diag) >= connect_length:
+        for coord in o_diag:
+            diag_word.append(board[coord[0]][coord[1]])
+    col = check_sequence(word)
+    diag = check_sequence(diag_word)
+    return col or diag
+
+def check_winner(board: list) -> str:
+    for i, row in enumerate(board):
+        result = check_sequence(row)
+        if result is not None:
+            return result
+        col_result = check_col(board, i, 4)
+        if col_result is not None:
+            return col_result
+
+
+def play(board: list, player: str, row: int, col: int):
+    updated = place_piece(player, row, col, board)
+    winner = check_winner(updated)
+    if winner:
+        return winner
 
 def display_board():
     pass
--- test_connect4.py
+++ test_connect4.py
@@ -1,4 +1,4 @@
-from connect4 import init_board, place_piece
+from connect4 import init_board, place_piece, check_winner, play
 
 def test_init_board():
     board = init_board(4, 3)
@@ -18,3 +18,88 @@ def test_place_piece_success():
     board = init_board(6, 7)
     new_board = place_piece('o', 0, 0, board)
     assert new_board[0][0] == 'o'
+
+
+def test_check_winner_none():
+    board = init_board(6,7)
+    assert check_winner(board) == None
+
+
+def test_check_winner_row_0():
+    board = init_board(6,7)
+    board[0] = ['.', '.', '.', 'x', 'x', 'x', 'x']
+    assert check_winner(board) == 'x'
+
+def test_check_winner_col_0():
+    board = init_board(6, 7)
+    board[0][0] = 'o'
+    board[1][0] = 'o'
+    board[2][0] = 'o'
+    board[3][0] = 'o'
+    assert check_winner(board) == 'o'
+
+def test_check_winner_col_4_row_2():
+    board = init_board(6, 7)
+    board[1][4] = 'o'
+    board[2][4] = 'o'
+    board[3][4] = 'o'
+    board[4][4] = 'o'
+    assert check_winner(board) == 'o'
+
+
+def test_check_winner_col_4_row_3():
+    board = init_board(6, 7)
+    board[2][4] = 'o'
+    board[3][4] = 'o'
+    board[4][4] = 'o'
+    board[5][4] = 'o'
+    assert check_winner(board) == 'o'
+
+def test_check_winner_col_4_row_6_no_exception():
+    board = init_board(6, 7)
+    board[2][5] = 'o'
+    assert check_winner(board) == None
+
+def test_check_winner_diag_row_0():
+    board = init_board(6, 7)
+    board[0][0] = 'o'
+    board[1][1] = 'o'
+    board[2][2] = 'o'
+    board[3][3] = 'o'
+    assert check_winner(board) == 'o'
+
+
+def test_check_winner_diag_row_1():
+    board = init_board(6, 7)
+    board[1][0] = 'x'
+    board[2][1] = 'x'
+    board[3][2] = 'x'
+    board[4][3] = 'x'
+    assert check_winner(board) == 'x'
+
+def test_check_winner_diag_row_1_col_1():
+    board = init_board(6, 7)
+    board[1][1] = 'x'
+    board[2][2] = 'x'
+    board[3][3] = 'x'
+    board[4][4] = 'x'
+    assert check_winner(board) == 'x'
+
+def test_check_winner_diag_row_1_col_0():
+    board = init_board(6, 7)
+    board[1][0] = 'o'
+    board[2][1] = 'o'
+    board[3][2] = 'o'
+    board[4][3] = 'o'
+    assert check_winner(board) == 'o'
+
+def test_play():
+    board = init_board(6, 7)
+    play(board, 'o', 0, 0)
+    play(board, 'x', 1, 0)
+    play(board, 'o', 1, 1)
+    play(board, 'x', 2, 0)
+    play(board, 'o', 2, 2)
+    play(board, 'x', 3, 0)
+    winner = play(board, 'o', 3, 3)
+    assert winner == 'o'
